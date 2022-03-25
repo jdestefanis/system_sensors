b5463c99b530b8405b6b64643c85dcd6a837a585
Javier Destefanis <javierdestefanis@hotmail.com>
2022-03-25 19:36:07 -0300

    Small modifications

--- change-mosquitto-hostname.sh
+++ change-mosquitto-hostname.sh
@@ -4,10 +4,9 @@ hostname1=$(cat /home/pi/system_sensors/src/settings.yaml | grep hostname | cut 
 hostname1=`echo $hostname1 | sed 's/ *$//g'`
 hostname1=$(echo $hostname1 | cut -d " " -f1)
 #echo "El hostname actual es: $hostname1"
-echo Ingrese el nuevo hostname o ip de home assistant
+echo Ingrese el nuevo hostname o IP Address de Home Assistant: default: homeassistant
 read hostname2
-#echo sed -i 's/hostname='$hostname1'/hostname='$hostname2'/g'
-echo "Esta seguro de querer cambiar el hostname a $hostname2?"
+echo "El hostname ingresado $hostname2 es correcto?"
 select yn in "Yes" "No"; do
 case $yn in
         Yes ) sudo sed -i 's/hostname: '$hostname1'/hostname: '$hostname2'/g' /home/pi/system_sensors/src/settings.yaml; echo "El nuevo hostname es: $hostname2"; break;;
@@ -19,9 +18,9 @@ user1=$(cat /home/pi/system_sensors/src/settings.yaml | grep user | cut -d ":" -
 user1=`echo $user1 | sed 's/ *$//g'`
 user1=$(echo $user1 | cut -d " " -f1)
 #echo "El user actual es: $user1"
-echo Ingrese el nuevo user del server de mqtt de home assistant
+echo Ingrese el usuario del servidor de mqtt configurado en Home Assistant
 read user2
-echo "Esta seguro de querer cambiar el user a $user2?"
+echo "El usuario ingresado $user2 es correcto?"
 select yn in "Yes" "No"; do
 case $yn in
         Yes ) sudo sed -i 's/user: '$user1'/user: '$user2'/g' /home/pi/system_sensors/src/settings.yaml; echo "El nuevo user es: $user2"; break;;
@@ -32,10 +31,9 @@ done
 pass1=$(cat /home/pi/system_sensors/src/settings.yaml | grep password | cut -d ":" -f 2)
 pass1=`echo $pass1 | sed 's/ *$//g'`
 pass1=$(echo $pass1 | cut -d " " -f1)
-#echo "El pass actual es: $pass1"
-echo Ingrese el nuevo pass del server de mqtt de home assistant
+echo Ingrese el password del server de mqtt configurado en de Home Assistant
 read pass2
-echo "Esta seguro de querer cambiar el password a $pass2?"
+echo "El password ingresado $pass2 es correcto?"
 select yn in "Yes" "No"; do
 case $yn in
         Yes ) sudo sed -i 's/password: '$pass1'/password: '$pass2'/g' /home/pi/system_sensors/src/settings.yaml; echo "El nuevo pass es: $pass2"; break;;
@@ -46,10 +44,9 @@ done
 devicename1=$(cat /home/pi/system_sensors/src/settings.yaml | grep deviceName | cut -d ":" -f 2)
 devicename1=`echo $devicename1 | sed 's/ *$//g'`
 devicename1=$(echo $devicename1 | cut -d " " -f1)
-#echo "El pass actual es: $devicename1"
-echo Ingrese el nuevo Server Name. Esto es solo como aparecera en Home Assistant.
+echo Ingrese un nombre de servicio. Este nombre es como Home Assistant lo identificara.
 read devicename2
-echo "Esta seguro de querer cambiar el Server Name a $devicename2?"
+echo "Esta nombre de servicio ingresado $devicename2 es correcto?"
 select yn in "Yes" "No"; do
 case $yn in
         Yes ) sudo sed -i 's/deviceName: '$devicename1'/deviceName: '$devicename2'/g' /home/pi/system_sensors/src/settings.yaml; echo "El nuevo server name es: $devicename2"; break;;
@@ -57,5 +54,31 @@ case $yn in
     esac
 done
 
+clientname1=$(cat /home/pi/system_sensors/src/settings.yaml | grep client_id | cut -d ":" -f 2)
+clientname1=`echo $clientname1 | sed 's/ *$//g'`
+clientname1=$(echo $clientname1 | cut -d " " -f1)
+echo Ingrese un nombre para el cliente del servicio. Este puede ser el mismo que el nombre de servicio: $devicename2
+read clientname2
+echo "Esta nombre de cliente ingresado $clientname2 es correcto?"
+select yn in "Yes" "No"; do
+case $yn in
+        Yes ) sudo sed -i 's/client_id: '$clientname1'/client_id: '$clientname2'/g' /home/pi/system_sensors/src/settings.yaml; echo "El nuevo server name es: $clientname2"; break;;
+        No ) exit;;
+    esac
+done
+
+timezone1=$(cat /home/pi/system_sensors/src/settings.yaml | grep timezone | cut -d ":" -f 2)
+timezone1=`echo $timezone1 | sed 's/ *$//g'`
+timezone1=$(echo $timezone1 | cut -d " " -f1)
+echo Ingrese el Timezone donde resize con el formato: America/Argentina/Buenos_Aires. Para otras zonas clickee aqui: https://www.php.net/manual/es/timezones.php
+read timezone2
+echo "Esta nombre de cliente ingresado $timezone2 es correcto?"
+select yn in "Yes" "No"; do
+case $yn in
+        Yes ) sudo sed -i 's/timezone: '$timezone1'/timezone: '$timezone2'/g' /home/pi/system_sensors/src/settings.yaml; echo "El nuevo server name es: $timezone2"; break;;
+        No ) exit;;
+    esac
+done
+
 sudo bash /home/pi/scripts_javier/mosquitto-hassio-devices.sh
-#exit 0
+echo "Configurando el cliente de Mosquitto.... Por favor espere."
